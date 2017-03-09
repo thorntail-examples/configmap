f942cb14253cc4e4e4f6f2ab1b68f38ca815b1e9
Heiko Braun <ike.braun@googlemail.com>
2017-03-09 10:47:49 +0100

    Cleanup and update to 2017.3.3

---
+++ src/test/java/org/obsidiantoaster/quickstart/configmap/GreetingServiceTest.java
@@ -0,0 +1,34 @@
+package org.obsidiantoaster.quickstart.configmap;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.arquillian.DefaultDeployment;
+
+/**
+ * @author Heiko Braun
+ */
+@RunWith(Arquillian.class)
+@DefaultDeployment
+public class GreetingServiceTest {
+
+    @Test
+    @RunAsClient
+    public void testResource() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080").path("greeting");
+
+        Response response = target.request(MediaType.APPLICATION_JSON).get();
+        Assert.assertEquals(200, response.getStatus());
+        Assert.assertTrue(response.readEntity(String.class).contains("Hello, World!"));
+    }
+
+}
