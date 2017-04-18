92ca38ff76f8a3bf8ed409301b6644a82bee23a0
Ken Finnigan <ken@kenfinnigan.me>
2017-04-18 10:37:07 -0400

    Cleanup local testing

--- src/test/java/io/openshift/boosters/GreetingServiceTest.java
+++ src/test/java/io/openshift/boosters/GreetingServiceTest.java
@@ -11,6 +11,8 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.arquillian.CreateSwarm;
 import org.wildfly.swarm.arquillian.DefaultDeployment;
 
 /**
@@ -19,6 +21,11 @@ import org.wildfly.swarm.arquillian.DefaultDeployment;
 @RunWith(Arquillian.class)
 @DefaultDeployment
 public class GreetingServiceTest {
+
+    @CreateSwarm
+    public static Swarm newContainer() throws Exception {
+        return new Swarm().withConfig(GreetingServiceTest.class.getClassLoader().getResource("project-defaults.yml"));
+    }
 
     @Test
     @RunAsClient
@@ -30,7 +37,7 @@ public class GreetingServiceTest {
 
         Response response = target.request(MediaType.APPLICATION_JSON).get();
         Assert.assertEquals(200, response.getStatus());
-        Assert.assertTrue(response.readEntity(String.class).contains("Hello Test!"));
+        Assert.assertTrue(response.readEntity(String.class).contains("Hello World from a ConfigMap!"));
     }
 
 }
