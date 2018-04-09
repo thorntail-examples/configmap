51f309cc70ba95d6df611700994fcb53cebd0e74
Tako Schotanus <tako@codejive.org>
2018-04-09 15:42:17 +0200

    refactor(build): Using new Jenkinsfile hook for OSIO (#36)

---
+++ .openshiftio/Jenkinsfile.setup.groovy
@@ -0,0 +1,10 @@
+
+setupEnvironmentPre(env) {
+  sh "if ! oc get -n ${env} configmap app-config -o yaml | grep app-config.yml; then oc create -n ${env} configmap app-config --from-file=app-config.yml; fi"
+}
+
+setupEnvironmentPost(env) {
+}
+
+return this
+
