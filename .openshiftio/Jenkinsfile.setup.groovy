c6ef0884f74bc381b9774c3d4b3544a424e3760c
Tako Schotanus <tako@codejive.org>
2018-04-11 15:11:27 +0200

    fix(hooks): Fixed typos in function definitions

--- .openshiftio/Jenkinsfile.setup.groovy
+++ .openshiftio/Jenkinsfile.setup.groovy
@@ -1,9 +1,9 @@
 
-setupEnvironmentPre(env) {
+def setupEnvironmentPre(env) {
   sh "if ! oc get -n ${env} configmap app-config -o yaml | grep app-config.yml; then oc create -n ${env} configmap app-config --from-file=app-config.yml; fi"
 }
 
-setupEnvironmentPost(env) {
+def setupEnvironmentPost(env) {
 }
 
 return this
